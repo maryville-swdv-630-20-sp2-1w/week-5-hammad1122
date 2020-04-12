2e76c4feadfa9bf5626abf7868c77230fa063c9a
Hammad Iqbal <49335367+hammad1122@users.noreply.github.com>
2020-04-12 16:40:38 -0500

    Create Facade.py

---
+++ Facade.py
@@ -0,0 +1,42 @@
+class Computer:
+    def getElectricShock(self):
+        print ("Ouch!")
+
+    def makeSound(self):
+        print ("Beep Beep!")
+
+    def showLoadingScreen(self):
+        print ("Loading...")
+
+    def bam(self):
+        print ("Ready to be used...")
+
+    def closeEverything(self):
+        print ("Bup bup bup buzzz!")
+
+    def sooth(self):
+        print ("Zzzzz")
+
+    def pullCurrent(self):
+        print ("Haaah!")
+        
+class ComputerFacade:
+    _computer = None
+
+    def __init__(self, computer):
+        self.computer = computer
+
+    def turnOn(self):
+        self.computer.getElectricShock()
+        self.computer.makeSound()
+        self.computer.showLoadingScreen()
+        self.computer.bam()
+
+    def turnOff(self):
+        self.computer.closeEverything()
+        self.computer.pullCurrent()
+        self.computer.sooth()
+        
+computer = ComputerFacade(Computer())
+computer.turnOn()
+computer.turnOff()
